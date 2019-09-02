dd000826a4df1d4df9aa49637778a86ae9a18420
shaowen <1063642821@qq.com>
2019-09-03 01:15:43 +0800

    zuul 网关

---
+++ Feign-Consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -0,0 +1,21 @@
+package com.example.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 客户端微服务
+ * @author shaowen
+ */
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
