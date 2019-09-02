3cd67afc5aa294e2c7c2c73520704d93a70af882
shaowen <1063642821@qq.com>
2019-09-02 23:11:59 +0800

    1

---
+++ Eureka-Cloud/src/main/java/com/example/cloud/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.cloud;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 关闭CSRF
+ * @author shaowen
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //关闭csrf
+        super.configure(http); //开启认证
+    }
+}
